1ce0b5f48306b1f7585ffd393de25ea5fa98bd9e
James Lamb <admin@oranged.to>
2021-03-18 00:55:21 +0000

    testing arm64 compile

--- src/crc32c_arm64_check.h
+++ src/crc32c_arm64_check.h
@@ -38,7 +38,7 @@ inline bool CanUseArm64Crc32() {
   // From 'arch/arm64/include/uapi/asm/hwcap.h' in Linux kernel source code.
   constexpr unsigned long kHWCAP_PMULL = 1 << 4;
   constexpr unsigned long kHWCAP_CRC32 = 1 << 7;
-  unsigned long hwcap = (&getauxval != nullptr) ? getauxval(AT_HWCAP) : 0;
+  unsigned long hwcap = getauxval(AT_HWCAP);
   return (hwcap & (kHWCAP_PMULL | kHWCAP_CRC32)) ==
          (kHWCAP_PMULL | kHWCAP_CRC32);
 #elif defined(__APPLE__)
